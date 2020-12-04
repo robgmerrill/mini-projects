97d4c638c3ca05bb232d48117c66a40eb00c49d1
robgmerrill <robgmerrill>
2020-12-03 16:22:24 -0800

    Team cards rough draft completed

--- expanding-cards-team/script.js
+++ expanding-cards-team/script.js
@@ -1,9 +1,26 @@
 // create variable that holds all the panels
+const panels = document.querySelectorAll('.panel');
+console.log(panels)
+
 
 // loop over all the panels
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        // alert("hello"); 
+        removeActiveClasses();
+        panel.classList.add('active');
+        console.log(panels)
+    })
+})
   // add event listener to each panel
     // call a custom removeActiveClass() function
     // call classList.add() method and add .active
 
 // create removeActiveClasses() function
     // loop over all the panels and call classList.remove() method on each panel removing .active class
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+
+}
