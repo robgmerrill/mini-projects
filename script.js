52acf9e12f267152f913f45299081122c31ef556
robgmerrill <robgmerrill>
2020-12-08 10:01:33 -0800

    Rotate nav project and starter comments for project

---
+++ script.js
@@ -0,0 +1,7 @@
+// target open id
+// target close id
+// target container class
+
+// event listener on open that adds show nav class to container
+
+// event listener on close that remove show nav class from container 
