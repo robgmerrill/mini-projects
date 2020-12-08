c3192a1fa26b420ddc53e07eebe50e6d8e685ea3
robgmerrill <robgmerrill>
2020-12-08 14:33:52 -0800

    Finished rotate nav comments projects

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // target open id
+const open = document.getElementById('open')
+console.log(open)
 // target close id
+const close = document.getElementById('close');
 // target container class
+const container = document.querySelector('.container');
 
 // event listener on open that adds show nav class to container
+open.addEventListener('click', () => {
+    container.classList.add('show-nav');
+})
+
 
 // event listener on close that remove show nav class from container 
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav')
+})
